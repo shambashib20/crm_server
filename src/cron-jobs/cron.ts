f2fbf3c221cda5f69d528f8b6dcb5c60dce0efb4
shambashib20 <shambashibmajumdar2000@gmail.com>
2025-07-05 15:30:00 +0530

    feat: code updated

--- src/cron-jobs/cron.ts
+++ src/cron-jobs/cron.ts
@@ -1,17 +1,7 @@
 import cron from "node-cron";
 import axios from "axios";
 
-cron.schedule("* * * * *", async () => {
-  console.log("Running FB sync task...");
-  try {
-    await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook", {});
-    console.log("Facebook leads fetched successfully.");
-  } catch (error) {
-    console.error("Error fetching leads:", error);
-  }
-});
-
-// cron.schedule("*/15 * * * *", async () => {
+// cron.schedule("* * * * *", async () => {
 //   console.log("Running FB sync task...");
 //   try {
 //     await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook", {});
@@ -20,3 +10,13 @@ cron.schedule("* * * * *", async () => {
 //     console.error("Error fetching leads:", error);
 //   }
 // });
+
+cron.schedule("*/15 * * * *", async () => {
+  console.log("Running FB sync task...");
+  try {
+    await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook", {});
+    console.log("Facebook leads fetched successfully.");
+  } catch (error) {
+    console.error("Error fetching leads:", error);
+  }
+});
