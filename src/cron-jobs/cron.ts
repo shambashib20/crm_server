a410c53b4ef2655099875919b9ba951e882a5d74
shambashib20 <shambashibmajumdar2000@gmail.com>
2025-07-14 11:50:14 +0530

    feat: cron job updated

--- src/cron-jobs/cron.ts
+++ src/cron-jobs/cron.ts
@@ -1,7 +1,17 @@
 import cron from "node-cron";
 import axios from "axios";
 
-cron.schedule("* * * * *", async () => {
+// cron.schedule("* * * * *", async () => {
+//   console.log("Running FB sync task...");
+//   try {
+//     await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook");
+//     console.log("Facebook leads fetched successfully.");
+//   } catch (error) {
+//     console.error("Error fetching leads:", error);
+//   }
+// });
+
+cron.schedule("*/15 * * * *", async () => {
   console.log("Running FB sync task...");
   try {
     await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook");
