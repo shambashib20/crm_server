84f92093d7a786c2304ae4856910564b1d490964
shambashib20 <shambashibmajumdar2000@gmail.com>
2025-07-14 12:44:02 +0530

    fix: fixing the cron job

--- src/cron-jobs/cron.ts
+++ src/cron-jobs/cron.ts
@@ -1,22 +1,22 @@
 import cron from "node-cron";
 import axios from "axios";
 
-// cron.schedule("* * * * *", async () => {
-//   console.log("Running FB sync task...");
-//   try {
-//     await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook");
-//     console.log("Facebook leads fetched successfully.");
-//   } catch (error) {
-//     console.error("Error fetching leads:", error);
-//   }
-// });
-
-cron.schedule("*/15 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
   console.log("Running FB sync task...");
   try {
-    await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook");
+    await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook", {});
     console.log("Facebook leads fetched successfully.");
   } catch (error) {
     console.error("Error fetching leads:", error);
   }
 });
+
+// cron.schedule("*/15 * * * *", async () => {
+//   console.log("Running FB sync task...");
+//   try {
+//     await axios.post("https://crm-server-tsnj.onrender.com/lead/webhook", {});
+//     console.log("Facebook leads fetched successfully.");
+//   } catch (error) {
+//     console.error("Error fetching leads:", error);
+//   }
+// });
